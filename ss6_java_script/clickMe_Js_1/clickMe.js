6953488f52b68f99623dddf08cec5067b921b602
Nguyễn Trần Nhật Minh <92421541+NhatMinhGit@users.noreply.github.com>
2024-09-19 17:12:40 +0700

    Create ss6_java_script and add clickMe! folder

---
+++ ss6_java_script/clickMe_Js_1/clickMe.js
@@ -0,0 +1,12 @@
+function showMessage1(){
+    alert('Xin chào! 1');
+}
+function showMessage2(){
+    document.getElementById('messageDiv').innerHTML = 'Xin chào! 2';
+}
+function showMessage3(){
+    document.write('Xin chào! 3');
+}
+function showMessage4(){
+    console.log('Xin chào! 4');
+}
