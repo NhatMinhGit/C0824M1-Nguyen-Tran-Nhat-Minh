67a8ce5a410a28e0d6faf6479a772f13ee9140ea
Nguyễn Trần Nhật Minh <92421541+NhatMinhGit@users.noreply.github.com>
2024-09-24 14:54:13 +0700

    Update files

--- ss8_if_else/Cau_Truc_Dieu_Kien_2/tinhSoNgay/tinhSoNgay.js
+++ ss8_if_else/Cau_Truc_Dieu_Kien_2/tinhSoNgay/tinhSoNgay.js
@@ -1,4 +1,4 @@
-function tinhSoNgay(){
+function tinhSoNgay_switchCase(){
     let thang = parseInt(document.getElementById("thang").value);
     
     switch (thang) {
@@ -40,5 +40,36 @@ function tinhSoNgay(){
             break;
         default:
             alert('Nhap sai roi!!!');
+    }
+}
+function tinhSoNgay_if_else() {
+    let thang = parseInt(document.getElementById("thang").value);
+    
+    if (thang === 1) {
+        document.write("Tháng 1 có 31 ngày");
+    } else if (thang === 2) {
+        document.write("Tháng 2 có 28 hoặc 29 ngày");
+    } else if (thang === 3) {
+        document.write("Tháng 3 có 31 ngày");
+    } else if (thang === 4) {
+        document.write("Tháng 4 có 30 ngày");
+    } else if (thang === 5) {
+        document.write("Tháng 5 có 31 ngày");
+    } else if (thang === 6) {
+        document.write("Tháng 6 có 30 ngày");
+    } else if (thang === 7) {
+        document.write("Tháng 7 có 31 ngày");
+    } else if (thang === 8) {
+        document.write("Tháng 8 có 31 ngày");
+    } else if (thang === 9) {
+        document.write("Tháng 9 có 30 ngày");
+    } else if (thang === 10) {
+        document.write("Tháng 10 có 31 ngày");
+    } else if (thang === 11) {
+        document.write("Tháng 11 có 30 ngày");
+    } else if (thang === 12) {
+        document.write("Tháng 12 có 31 ngày");
+    } else {
+        alert('Nhập sai rồi!!!');
     }
 }
